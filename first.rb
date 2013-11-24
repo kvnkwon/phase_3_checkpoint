e95c59153dbc6c1676f500fa66c63afec11fed01
Kevin <kvnkwon@gmail.com>
2013-11-24 16:08:12 -0500

    added more responses

--- first.rb
+++ first.rb
@@ -1,5 +1,7 @@
 def playback_input(user_input)
-  puts "You said: #{user_input}"  
+  puts "You said: #{user_input}"
+  puts "Ok, fine!" if user_input == "Nothing!" 
+  puts "I don\'t have time for that right now!" if user_input == "I have a lot to say" 
 end
 
 puts "What do you want to say?"
